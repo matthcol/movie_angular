0faf6ede7613434513c6705a46fff189442f376d
matthcol <matthias.colin@gmail.com>
2023-09-06 16:12:12 +0200

    basic auth

--- src/app/modules/movie/services/movie.service.ts
+++ src/app/modules/movie/services/movie.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Movie } from '../models/movie';
 import { environment } from 'src/environments/environment'; // swith auto avec .prod en prod
 import { Observable } from 'rxjs';
@@ -11,23 +11,28 @@ const endpoint = '/movie';
 })
 export class MovieService {
 
-  constructor(private http: HttpClient) { }
+  private auth_header: HttpHeaders;
+
+  constructor(private http: HttpClient) { 
+    let password: string = btoa(`admin:password`);
+    this.auth_header = new HttpHeaders(`Authorization: Basic ${password}`)
+  }
 
   getMovieAsPromise(id: number): Promise<Movie>{
-    return this.http.get<Movie>(`${environment.apiBaseUrl}${endpoint}/${id}`).toPromise();
+    return this.http.get<Movie>(`${environment.apiBaseUrl}${endpoint}/${id}`, {headers: this.auth_header}).toPromise();
   }
 
   getMoviesAsPromise(): Promise<Movie[]> {
-    return this.http.get<Movie[]>(`${environment.apiBaseUrl}${endpoint}`).toPromise();
+    return this.http.get<Movie[]>(`${environment.apiBaseUrl}${endpoint}`,{headers: this.auth_header}).toPromise();
   }
 
   getMoviesAsObservable(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${environment.apiBaseUrl}${endpoint}`);
+    return this.http.get<Movie[]>(`${environment.apiBaseUrl}${endpoint}`, {headers: this.auth_header});
   }
 
   createMovie(m: Movie): Observable<Movie> {
     console.log("send http post to create movie:");
     console.log(m);
-    return this.http.post<Movie>(`${environment.apiBaseUrl}${endpoint}`, m);
+    return this.http.post<Movie>(`${environment.apiBaseUrl}${endpoint}`, m, {headers: this.auth_header});
   }
 }
